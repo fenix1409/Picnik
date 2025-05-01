6fee8a961ddf59f14d96311881dba3ebfa59309c
fenix1409 <b2550486@gmail.com>
2025-05-01 18:59:25 +0500

    feat: Enhance SingleProduct component to handle loading and error states

--- src/components/pages/SinlgeProduct.tsx
+++ src/components/pages/SinlgeProduct.tsx
@@ -1,37 +1,44 @@
-import { useNavigate, useParams } from "react-router-dom"
-import getSinglePRoduct from "../service/getSinglePRoduct"
-import { Arrow, Stars2 } from "../../assets/Icons"
-import getProducts from "../service/getProducts"
-import { IMAGE_API } from "../hooks/getEnv"
-import { Product } from "../types/ProductType"
+import { useNavigate, useParams } from "react-router-dom";
+import getSinglePRoduct from "../service/getSinglePRoduct";
+import { Arrow, Stars2 } from "../../assets/Icons";
+import { IMAGE_API } from "../hooks/getEnv";
+import { Product } from "../types/ProductType";
+import getProducts from "../service/getProducts";
 
 const SinlgeProduct = () => {
   const { id } = useParams()
   const productid = Array.isArray(id) ? id[0] : id
-  const { data, isLoading } = getSinglePRoduct(productid)
-  console.log(data)
+  const { data, isLoading, isError } = getSinglePRoduct(productid)
   const navigate = useNavigate()
   const products: Product = getProducts()
+  console.log(products);
+
+
+  if (isLoading) return <div>Yuklanmoqda...</div>
+  if (isError || !data) return <div>Xatolik yuz berdi yoki ma'lumot yo'q</div>
 
   return (
     <div className="mt-[13px] px-[100px]">
-      <button className="inline-block cursor-pointer mb-[30px]" onClick={() => navigate(-1)}><Arrow /></button>
+      <button onClick={() => navigate(-1)} className="inline-block cursor-pointer mb-[30px]">
+        <Arrow />
+      </button>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-[18px]">
           <div className="w-[120px] my-auto">
-            <div className="w-[120px] h-[120px] bg-[#E9F8EC] rounded-[20px]">
-              <img src={`${IMAGE_API}/${products.image_src}`} alt="image" width={50} height={50} />
+            <div className="w-[120px] h-[120px] bg-[#E9F8EC] rounded-[20px] pt-[30px]">
+              <img src={`${IMAGE_API}/${products.image_src}`} alt="image" width={70} height={70} className="mx-auto" />
             </div>
           </div>
-          {/* <img src={`${IMAGE_API}/${data.image_src}`} alt="image" width={444} height={530} /> */}
+          <div className="w-[444px] h-[530px] bg-[#E9F8EC] pt-[100px] rounded-[20px]">
+            <img src={`${IMAGE_API}/${data.image_src}`} alt="image" width={300} height={330} className="mx-auto"/>
+          </div>
         </div>
         <div className="w-[612px]">
           <div className="space-y-[14px] mb-[43px]">
-            {/* <h1 className="text-[36px] leading-[100%] font-bold">{data.title}</h1>
+            <h1 className="text-[36px] leading-[100%] font-bold">{data.title}</h1>
             <div className=""><Stars2 /></div>
-            <span className="inline-block text-[32px] leading-[100%] font-bold">{data.price} so'm</span> */}
+            <span className="inline-block text-[32px] leading-[100%] font-bold">{data.price} so'm</span>
           </div>
-
         </div>
       </div>
     </div>
--- src/components/service/getSinglePRoduct.ts
+++ src/components/service/getSinglePRoduct.ts
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query"
 import { instance } from "../hooks/useAxios"
 
 const getSinglePRoduct = (id: string | undefined) => {
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryKey: ['singleProduct', id],
         queryFn: async () => (await instance().get(`/products/${id}`)).data.data
     })
-    return { data, isLoading }
+    return { data, isLoading, isError }
 }
 
 export default getSinglePRoduct
